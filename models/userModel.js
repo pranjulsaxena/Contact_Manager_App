a8839c8e105be7c06db4cd8e4e23e25d01a3bddf
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-02-05 02:56:13 +0530

    userModel

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("express");
+const { applyTimestamps } = require("./contactModels");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+   username:{
+    type:String,
+    required: [true,"Please add the user name"]
+   },
+   email:{
+        type:String,
+        required:[true,"Please add the user email address"],
+        unique:[true,"Email address already taken"]
+   },
+   password: {
+    type:String,
+    required:[true,"Please add the user password"]
+   }
+},{
+    timestamps:true,
+})
+
+module.exports = mongoose.model("User",userSchema);
